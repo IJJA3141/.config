55d8ee366ec1be3635aae355414a024a091d6b86
IJJA3141 <alex.estella.cj@gmail.com>
2023-07-30 00:05:45 +0200

    00.03 lsa

--- PowerShell/user_profile.ps1
+++ PowerShell/user_profile.ps1
@@ -7,6 +7,9 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tiwahu.omp.json" | Invoke-E
 -- hul10
 #>
 
+# Function
+function lsall { Get-ChildItem $args -Force }
+
 # Alias
-Set-Alias vcpkg "D:\Program Files\Vcpkg\vcpkg.exe"
-Set-Alias lsa ls --all
+Set-Alias vcpkg 'D:\Program Files\Vcpkg\vcpkg.exe'
+Set-Alias lsa lsall
