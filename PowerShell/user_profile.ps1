0c7dd261ebcd3c21a8f97bc80e48585e994ed490
IJJA3141 <alex.estella.cj@gmail.com>
2023-08-06 04:47:37 +0200

    00.12 Copy-Filtered

--- PowerShell/user_profile.ps1
+++ PowerShell/user_profile.ps1
@@ -9,6 +9,24 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tiwahu.omp.json" | Invoke-E
 
 # Function
 function lsall { Get-ChildItem $args -Force }
+function Copy-Filtered { # from https://serverfault.com/questions/707085/copy-item-with-multiple-filter
+    param (
+        [string] $Source,
+        [string] $Target,
+        [string[]] $Filter
+    )
+    $ResolvedSource = Resolve-Path $Source
+    $NormalizedSource = $ResolvedSource.Path.TrimEnd([IO.Path]::DirectorySeparatorChar) + [IO.Path]::DirectorySeparatorChar
+    Get-ChildItem $Source -Include $Filter -Recurse | ForEach-Object {
+        $RelativeItemSource = $_.FullName.Replace($NormalizedSource, '')
+        $ItemTarget = Join-Path $Target $RelativeItemSource
+        $ItemTargetDir = Split-Path $ItemTarget
+        if (!(Test-Path $ItemTargetDir)) {
+            [void](New-Item $ItemTargetDir -Type Directory)
+        }
+        Copy-Item $_.FullName $ItemTarget
+    }
+}
 
 # Alias
 Set-Alias vcpkg 'D:\Program Files\Vcpkg\vcpkg.exe'
