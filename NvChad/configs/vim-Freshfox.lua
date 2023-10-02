08627298fdd909943e04f6b1f76a173347e06ba2
IJJA3141 <alex.estella.cj@gmail.com>
2023-10-02 19:14:31 +0200

    00.00 init

--- NvChad/configs/vim-Freshfox.lua
+++
@@ -1,9 +0,0 @@
-local Freshfox = require("vim-Freshfox")
-
-Freshfox.setup()
-
-vim.api.nvim_create_autocmd("User", {
-	pattern = "VimtexEventCompileSuccess",
-	callback = Freshfox.refresh,
-	group = vim.api.nvim_create_augroup("vimtex_refresh_firefox", { clear = true }),
-})
