4af69c4da70a74e13b478f360a50a89adcc616b4
IJJA3141 <alex.estella.cj@gmail.com>
2023-12-12 11:09:18 +0100

    04.03 all modules

--- eww/scripts/layout.sh
+++ eww/scripts/layout.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-while true; do
-	im=$(fcitx5-remote -n)
+im=$(fcitx5-remote -n)
 
-	if [ "$im" = "keyboard-us" ]; then
-    echo "(box :class \"layout\" \"US\")"
-	elif [ "$im" = "mozc" ]; then
-    echo "(box :class \"layout\" \"JP\")"
-	else
-    echo "(box :class \"layout\" \"FR\")"
-	fi
+if [ "$im" = "mozc" ]; then
+	fcitx5-remote -s keyboard-ch-fr
+elif [ "$im" = "keyboard-us" ]; then
+	fcitx5-remote -s mozc
+else
+	fcitx5-remote -s keyboard-us
+fi
 
-  sleep 2
-done
+im2=$(fcitx5-remote -n)
+
+if [ "$im2" = "mozc" ]; then
+	eww update 'k=(box "JP")'
+elif [ "$im2" = "keyboard-us" ]; then
+	eww update 'k=(box "US")'
+else
+	eww update 'k=(box "FR")'
+fi
