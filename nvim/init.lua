e9af893db499c40c57742b3b3c5d6d29fbd54550
IJJA3141 <alex.estella.cj@gmail.com>
2024-01-31 19:50:58 +0100

    08.02 nvim rm useless function

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,11 +27,7 @@ vim.cmd.colorscheme("gruvbox")
 
 vim.cmd([[
 function OpenMarkdownPreview (url)
-if system("ps -e | grep -c firefox")>= 1
 execute "silent ! firefox-developer-edition --new-window " . a:url
-else
-execute "silent ! firefox-developer-edition " . a:url
-endif
 endfunction
 
 let g:mkdp_browserfunc = 'OpenMarkdownPreview'
