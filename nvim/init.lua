55a2246c11c072b72cfebc04d5763b98f3425bf9
IJJA3141 <alex.estella.cj@gmail.com>
2024-02-10 01:38:11 +0100

    10.00 nvim dap

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,8 +27,12 @@ vim.cmd.colorscheme("gruvbox")
 
 vim.cmd([[
 function OpenMarkdownPreview (url)
+if system("ps -e | grep -c firefox")>= 1
 execute "silent ! firefox-developer-edition --new-window " . a:url
+else
+echo system("firefox-developer-edition &")
+execute "silent ! firefox-developer-edition " . a:url
+endif
 endfunction
-
 let g:mkdp_browserfunc = 'OpenMarkdownPreview'
 ]])
