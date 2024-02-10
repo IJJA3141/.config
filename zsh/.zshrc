af509f52c41580a2967b7630fb4d8d9a53c5cbb9
IJJA3141 <alex.estella.cj@gmail.com>
2024-02-10 19:14:21 +0100

    11.00 zsh

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,6 +1,7 @@
+source ~/.config/zsh/utility.zsh
 source ~/.config/zsh/plugins.zsh
+source ~/.config/zsh/options.zsh
 source ~/.config/zsh/history.zsh
 source ~/.config/zsh/style.zsh
 source ~/.config/zsh/alias.zsh
-source ~/.config/zsh/options.zsh
 source ~/.config/zsh/keybinds.zsh
--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -1,28 +1,28 @@
-bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[B" history-beginning-search-forward
-
-#   Nvim mode
-bindkey -v
-export KEYTIMEOUT=1
-
-# Use vim keys in tab complete menu:
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' history-beginning-search-backward
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'j' history-beginning-search-forward
-
-# Change cursor shape for different vi modes.
-function zle-keymap-select {
-  if [[ ${KEYMAP} == vicmd ]] ||
-     [[ $1 = 'block' ]]; then
-    echo -ne '\e[1 q'
-  elif [[ ${KEYMAP} == main ]] ||
-       [[ ${KEYMAP} == viins ]] ||
-       [[ ${KEYMAP} = '' ]] ||
-       [[ $1 = 'beam' ]]; then
-    echo -ne '\e[5 q'
-  fi
-}
+# bindkey "^[[A" history-beginning-search-backward
+# bindkey "^[[B" history-beginning-search-forward
+# 
+# #   Nvim mode
+# bindkey -v
+# export KEYTIMEOUT=1
+# 
+# # Use vim keys in tab complete menu:
+# bindkey -M menuselect 'h' vi-backward-char
+# bindkey -M menuselect 'k' history-beginning-search-backward
+# bindkey -M menuselect 'l' vi-forward-char
+# bindkey -M menuselect 'j' history-beginning-search-forward
+# 
+# # Change cursor shape for different vi modes.
+# function zle-keymap-select {
+#   if [[ ${KEYMAP} == vicmd ]] ||
+#      [[ $1 = 'block' ]]; then
+#     echo -ne '\e[1 q'
+#   elif [[ ${KEYMAP} == main ]] ||
+#        [[ ${KEYMAP} == viins ]] ||
+#        [[ ${KEYMAP} = '' ]] ||
+#        [[ $1 = 'beam' ]]; then
+#     echo -ne '\e[5 q'
+#   fi
+# }
 #
 # zle -N zle-keymap-select
 # zle-line-init() {
@@ -32,3 +32,5 @@ function zle-keymap-select {
 # zle -N zle-line-init
 # echo -ne '\e[5 q' # Use beam shape cursor on startup.
 # preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+bindkey -v
