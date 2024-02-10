f94fb9ad734dde2bcc6a4ff4efd39c3a5f9a9ae7
IJJA3141 <alex.estella.cj@gmail.com>
2024-02-10 18:18:46 +0100

    10.02 zsh ls format

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,3 +1,3 @@
-alias lsa="ls -ahl --color=auto"
+alias lsa="ls -ahl --color=auto --group-directories-first"
 alias ls="ls --color=auto"
 alias gr="git rm . --cached -rf >> /dev/null && git add . >> /dev/null && git ls-files"
