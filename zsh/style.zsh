cb392877bc251d535d7035569341d74d39bbb60a
IJJA3141 <alex.estella.cj@gmail.com>
2024-03-02 19:01:49 +0100

    14.04 zsh

--- zsh/style.zsh
+++ zsh/style.zsh
@@ -5,6 +5,7 @@ autoload -U colors && colors
 #eval "$(oh-my-posh init zsh --config /usr/share/oh-my-posh/themes/takuya.omp.json)"
 #eval "$(oh-my-posh init zsh --config /usr/share/oh-my-posh/themes/tiwahu.omp.json)"
 
+## Custom theme
 eval "$(oh-my-posh init zsh --config ~/.config/zsh/posh/ijja.omp.json)"
 
 eval "$(dircolors ~/.config/zsh/.dir_colors)"
