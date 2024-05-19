7f9b81d9a14d3337d308e51def20cf6d62fc0ae6
IJJA3141 <alex.estella.cj@gmail.com>
2024-05-19 19:00:07 +0200

    18 naming ?

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -3,36 +3,4 @@ setopt autocd
 setopt extendedglob
 unsetopt beep
 
-### Basic auto/tab complete:
-autoload -U compinit
-zstyle ':completion:*' menu select
-zmodload zsh/complist
-compinit
-#   Include hidden files.
-_comp_options+=(globdots)
 
-zmodload zsh/zle
-zmodload zsh/zpty
-zmodload zsh/complist
-
-autoload _vi_search_fix
-autoload -Uz colors
-autoload -U compinit
-colors
-
-zle -N _vi_search_fix
-zle -N _sudo_command_line
-
-# Completion
-# disable sort when completing `git checkout`
-zstyle ':completion:*:git-checkout:*' sort false
-# set descriptions format to enable group support
-zstyle ':completion:*:descriptions' format '[%d]'
-# set list-colors to enable filename colorizing
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-
-# Autosuggestion
-ZSH_AUTOSUGGEST_USE_ASYNC="true"
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor regexp root line)
-ZSH_HIGHLIGHT_MAXLENGTH=512
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=$color8,bold"
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,40 +1,30 @@
-# Configure and load plugins using Zinit's
-ZINIT_HOME="${ZINIT_HOME:-${XDG_DATA_HOME:-${HOME}/.local/share}/zinit}"
-
-# Added by Zinit's installer
-if [[ ! -f ${ZINIT_HOME}/zinit.git/zinit.zsh ]]; then
+# zinit
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git}"
+if [[ ! -f ${ZINIT_HOME}/zinit.zsh ]]; then
     print -P "%F{14}▓▒░ Installing Flexible and fast ZSH plugin manager %F{13}(zinit)%f"
     command mkdir -p "${ZINIT_HOME}" && command chmod g-rwX "${ZINIT_HOME}"
-    command git clone https://github.com/zdharma-continuum/zinit.git "${ZINIT_HOME}/zinit.git" && \
+    command git clone https://github.com/zdharma-continuum/zinit.git "${ZINIT_HOME}" && \
         print -P "%F{10}▓▒░ Installation successful.%f%b" || \
         print -P "%F{9}▓▒░ The clone has failed.%f%b"
 fi
 
-source "${ZINIT_HOME}/zinit.git/zinit.zsh"
+source "${ZINIT_HOME}/zinit.zsh"
+zstyle ':completion:*' menu select
+_comp_options+=(globdots)
 
-zinit ice blockf atpull'zinit creinstall -q .'
+
 zinit light zsh-users/zsh-completions
 
-autoload compinit
-compinit
+autoload compinit && compinit
 
 zinit light-mode for \
-    hlissner/zsh-autopair \
     zdharma-continuum/fast-syntax-highlighting \
-    MichaelAquilina/zsh-you-should-use \
     zsh-users/zsh-autosuggestions \
+    hlissner/zsh-autopair \
     softmoth/zsh-vim-mode \
 
-    zinit ice wait'3' lucid
-zinit light zsh-users/zsh-history-substring-search
-
-zinit ice wait'2' lucid
-zinit light zdharma-continuum/history-search-multi-word
-
-# EXA
-zinit ice wait lucid from"gh-r" as"program" mv"bin/exa* -> exa"
-zinit light ogham/exa
-
-# BAT
-zinit ice wait lucid from"gh-r" as"program" mv"*/bat -> bat" atload"export BAT_THEME='Nord'"
-zinit light sharkdp/bat
+# Autosuggestion
+ZSH_AUTOSUGGEST_USE_ASYNC="true"
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor regexp root line)
+ZSH_HIGHLIGHT_MAXLENGTH=512
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=$color8,bold"
