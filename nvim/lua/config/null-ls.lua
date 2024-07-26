97cd375488c8df7eb651d730f17e76a2971677d0
IJJA3141 <alex.estella.cj@gmail.com>
2024-07-26 19:04:23 +0200

    00.01.05 nvim

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -4,20 +4,20 @@ local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
 local sources = {
-  formatting.beautysh,
-  formatting.black,
-  formatting.clang_format,
-  formatting.prettier,
-  formatting.prettierd,
-  formatting.stylua,
-  --formatting.shfmt,
+	formatting.beautysh,
+	formatting.black,
+	formatting.clang_format,
+	formatting.prettier,
+	formatting.prettierd,
+	formatting.stylua,
+	--formatting.shfmt,
 
-  --lint.cpplint, annoying
-  lint.luacheck,
-  lint.shellcheck,
+	--lint.cpplint, annoying
+	lint.luacheck,
+	lint.shellcheck,
 }
 
 null_ls.setup({
-  debug = true,
-  sources = sources,
+	debug = true,
+	sources = sources,
 })
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,36 +1,8 @@
 return {
 	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-		window = {
-			border = "single",
-		},
-		plugins = { spelling = true },
-		defaults = {
-			mode = { "n", "v" },
-			["g"] = { name = "+goto" },
-			["gs"] = { name = "+surround" },
-			["]"] = { name = "+next" },
-			["["] = { name = "+prev" },
-			["t"] = { name = "Telescope" },
-			["<leader><tab>"] = { name = "+tabs" },
-			["<leader>b"] = { name = "+buffer" },
-			["<leader>c"] = { name = "+code" },
-			["<leader>f"] = { name = "+file/find" },
-			["<leader>g"] = { name = "+git" },
-			["<leader>h"] = { name = "+harpoon" },
-			["<leader>gh"] = { name = "+hunks" },
-			["<leader>q"] = { name = "+quit/session" },
-			["<leader>s"] = { name = "+search" },
-			["<leader>u"] = { name = "+ui" },
-			["<leader>w"] = { name = "+windows" },
-			["<leader>x"] = { name = "+diagnostics/quickfix" },
-			["<leader>z"] = { name = "+zenmode" },
-		},
+	dependencies = {
+		"echasnovski/mini.icons",
+		"nvim-tree/nvim-web-devicons",
 	},
-	config = function(_, opts)
-		local wk = require("which-key")
-		wk.setup(opts)
-		wk.register(opts.defaults)
-	end,
+	event = "VeryLazy",
 }
