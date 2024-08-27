8ef61336ccc0256703d86a45856e165cc49203f9
IJJA3141 <alex.estella.cj@gmail.com>
2024-08-27 19:43:50 +0200

    00.01.13 save

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -20,22 +20,20 @@ return {
 						type = "cppdbg",
 						request = "launch",
 						program = function()
-							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/out/Debug/", "file")
 						end,
 						cwd = "${workspaceFolder}",
 						stopAtEntry = true,
 					},
 					{
-						name = "Attach to gdbserver :1234",
+						name = "Launch file sudo",
 						type = "cppdbg",
 						request = "launch",
-						MIMode = "gdb",
-						miDebuggerServerAddress = "localhost:1234",
-						miDebuggerPath = "/usr/bin/gdb",
-						cwd = "${workspaceFolder}",
 						program = function()
-							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/out/Debug/", "file")
 						end,
+						cwd = "${workspaceFolder}",
+						stopAtEntry = true,
 					},
 				}
 			end,
