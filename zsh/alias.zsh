5ad807f2e138e1b7a9e1990cd9edd1d83605c892
IJJA3141 <alex.estella.cj@gmail.com>
2024-09-01 23:42:25 +0200

    00.02.00 save zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,4 +1,7 @@
 alias aliaslist="cat ~/.config/zsh/alias.zsh"
+alias keybinds="cat ~/.config/hypr/input.conf"
+alias wifi="zsh ~/.config/scripts/fuckwifi.sh"
+alias ssh="kitten ssh"
 
 alias lsa="ls -vahl --color=auto --group-directories-first"
 alias ls="ls --color=auto"
@@ -25,3 +28,14 @@ function lc ()
     echo "line: ${l}\ncolomn: ${c}"
 }
 alias lc=lc
+
+function forallfiles()
+{
+    files=($(find ./ -exec ls -d $PWD/{} \;))
+
+    for ((i = 2 ; i <= ${#files[@]} ; i++ )); do
+        echo "$1 $files[i]"
+        eval "$1 $files[i]"
+    done
+}
+alias forfiles=forallfiles
