78f3bfae0d157197c736087de0a9e196ead4456e
IJJA3141 <alex.estella.cj@gmail.com>
2025-03-23 21:55:00 +0100

    00.03.14 eww notification

--- eww/notification.py
+++ eww/notification.py
@@ -13,6 +13,12 @@ import dbus
 import time
 import os
 
+# The reason the notification was closed.
+# 1 - The notification expired.
+# 2 - The notification was dismissed by the user.
+# 3 - The notification was closed by a call to CloseNotification.
+# 4 - Undefined/reserved reasons.
+
 
 def save_img_byte(px_args, save_path):
     GdkPixbuf.Pixbuf.new_from_bytes(
@@ -24,49 +30,58 @@ def save_img_byte(px_args, save_path):
         rowstride=px_args[2],
         bits_per_sample=px_args[4],
     ).savev(save_path, "png")
-
     return
 
 
 def wait(notification_server, timeout, notification_id):
     time.sleep(timeout / 1000)
     notification_server.remove_notification(notification_id, 1)
-
     return
 
 
 class Notification:
-    def __init__(self, app_name, app_icon, summary, body):
+    def __init__(self, id, app_name, app_icon, summary, body, actions):
+        self.id = id
         self.app_name = app_name
         self.app_icon = app_icon
         self.summary = summary
         self.body = body
+        self.actions = actions
+        return
+
+    def print_actions(self):
+        string = ""
+
+        for i in range(0, len(self.actions), 2):
+            string += f""" (action :notification_id "{str(self.id)}" :action_key "{self.actions[i]}" :text "{self.actions[i + 1]}")"""
+
+        return string
 
 
 class NotificationServer(dbus.service.Object):
     def __init__(self):
-        bus_name = dbus.service.BusName(
-            "org.freedesktop.Notifications", bus=dbus.SessionBus()
-        )
+        bus_name = dbus.service.BusName("org.freedesktop.Notifications", bus=dbus.SessionBus())
         dbus.service.Object.__init__(self, bus_name, "/org/freedesktop/Notifications")
 
-        self.notifications = {}
         self.default_timeout = 10000
-        self.id = 0
+        self.notifications = {}
         self.is_mute = False
+        self.id = 0
 
         if os.path.exists("/tmp/notification-server"):
             shutil.rmtree("/tmp/notification-server")
+
         os.mkdir("/tmp/notification-server")
+        return
 
     # Signals
     @dbus.service.signal("org.freedesktop.notifications", signature="uu")
     def NotificationClosed(self, notification_id, reason):
-        return
+        return (notification_id, reason)  # ?
 
     @dbus.service.signal("org.freedesktop.notifications", signature="us")
     def ActionInvoked(self, notification_id, action_key):
-        return
+        return (notification_id, action_key)
 
     # Methods that just need to be here
     @dbus.service.method("org.freedesktop.Notifications", in_signature="u")
@@ -82,27 +97,13 @@ class NotificationServer(dbus.service.Object):
     def GetCapabilities(self):
         return ("actions", "body", "body-hyprlinks", "icon-static")
 
-    @dbus.service.method(
-        "org.freedesktop.Notifications", in_signature="susssasa{sv}i", out_signature="u"
-    )
-    def Notify(
-        self,
-        app_name,
-        replaces_id,
-        app_icon,
-        summary,
-        body,
-        actions,
-        hints,
-        timeout,
-    ):
+    @dbus.service.method("org.freedesktop.Notifications", in_signature="susssasa{sv}i", out_signature="u")
+    def Notify(self, app_name, replaces_id, app_icon, summary, body, actions, hints, timeout,):
         if app_name == "toggle-mute":
             self.is_mute = not self.is_mute
 
             if self.is_mute:
-                os.system(
-                    "eww update bell_listener=\"(icon :class_name 'red' :icon '')\""
-                )
+                os.system("eww update bell_listener=\"(icon :class_name 'red' :icon '')\"")
             else:
                 os.system("eww update bell_listener=\"(icon :icon '')\"")
             return 0
@@ -112,59 +113,52 @@ class NotificationServer(dbus.service.Object):
 
         if app_name == "rm-msg":
             if replaces_id:
-                self.ActionInvoked(replaces_id, "activate")
                 self.remove_notification(replaces_id, 2)
+                if summary:
+                    self.ActionInvoked(replaces_id, summary)
             return 0
 
         if replaces_id != 0:
-            self.remove_notification(replaces_id, 1)
+            id = replaces_id
+        else:
+            self.id += 1
+            id = self.id
 
-        self.id += 1
-        self.notifications[self.id] = Notification(app_name, app_icon, summary, body)
+        self.notifications[id] = Notification(id, app_name, app_icon, summary, body, actions)
 
         if "image-data" in hints:
-            save_img_byte(
-                hints["image-data"], f"/tmp/notification-server/{self.id}.png"
-            )
-            self.notifications[self.id].app_icon = (
-                f"/tmp/notification-server/{self.id}.png"
-            )
+            save_img_byte(hints["image-data"], f"/tmp/notification-server/{id}.png")
+            self.notifications[id].app_icon = (f"/tmp/notification-server/{id}.png")
 
         self.print_state()
 
-        if timeout == -1:
-            timer_thread = threading.Thread(
-                target=wait, args=(self, self.default_timeout, self.id)
-            )
-            timer_thread.start()
-        elif timeout != 0:
-            timer_thread = threading.Thread(target=wait, args=(self, timeout, self.id))
-            timer_thread.start()
+        if timeout == 0:
+            return 0
 
-        return self.id
+        if timeout == -1:
+            timeout = self.default_timeout
+
+        timer_thread = threading.Thread(target=wait, args=(self, timeout, id))
+        timer_thread.start()
+        return id
 
     def remove_notification(self, notification_id, reason):
         if self.notifications.get(notification_id):
             self.notifications.pop(notification_id)
             self.print_state()
             self.NotificationClosed(notification_id, reason)
-
         return
 
     def print_state(self):
         string = ""
-        i = 0
 
-        for notification_key in self.notifications:
-            if self.notifications[notification_key].app_icon:
-                str = self.notifications[notification_key].app_icon
+        for i in self.notifications:
+            if self.notifications[i].app_icon:
+                str = self.notifications[i].app_icon
             else:
                 str = ""
 
-            string = (
-                string
-                + f"""(notification :id {notification_key} :app_name '{self.notifications[notification_key].app_name or ''}' :summary "{self.notifications[notification_key].summary or ''}" :body "{self.notifications[notification_key].body or ''}" :icon '{str}' )"""
-            )
+            string = (string + f"""(notification :id {i} :app_name '{self.notifications[i].app_name or ''}' :summary "{self.notifications[i].summary or ''}" :body "{self.notifications[i].body or ''}" :icon '{str}' :actions '{self.notifications[i].print_actions()}')""")
         if string:
             string = string.replace("\n", "")
             print(
